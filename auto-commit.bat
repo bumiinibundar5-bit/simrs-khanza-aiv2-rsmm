239be20ab7c8be0dc618d3b3333b80ec493ab1e7
sadarsmkn2mjn <sadar05@guru.smk.belajar.id>
2026-03-07 20:00:37 +0800

    auto backup commit

---
+++ auto-commit.bat
@@ -0,0 +1,15 @@
+@echo off
+echo ==============================
+echo AUTO COMMIT SIMRS KHANZA
+echo ==============================
+
+git add .
+
+git commit -m "auto backup commit"
+
+git push
+
+echo ==============================
+echo SELESAI PUSH KE GITHUB
+echo ==============================
+pause
