79cfafe8e0bfdd1efef078898be3378f00da1df7
ipin <bumiinibundar5@gmail.com>
2026-04-12 22:39:13 +0800

    auto update rsmm

--- auto-commit.bat
+++ auto-commit.bat
@@ -1,15 +1,17 @@
 @echo off
-echo ==============================
-echo AUTO COMMIT SIMRS KHANZA
-echo ==============================
+cd /d D:\SIMRS2026\simrs-khanza-aiv2-rsmm
+
+echo ==========================
+echo PUSH REPO RSMM
+echo ==========================
 
 git add .
 
-git commit -m "auto backup commit"
+git commit -m "auto update rsmm"
 
-git push
+git push rsmm main
 
-echo ==============================
-echo SELESAI PUSH KE GITHUB
-echo ==============================
+echo ==========================
+echo SELESAI PUSH RSMM
+echo ==========================
 pause
